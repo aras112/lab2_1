634d00d1f4aff98eeeeaeebb78430226e346cf74
aras112nowe@o2.pl <9018a9de>
2019-03-20 18:50:49 +0100

    testy_v1_dodano_testy_1.3_punkt

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -11,10 +11,45 @@ public class BinarySearchTest {
     @Before public void setUp() throws Exception {
     }
 
-    @Test public void presentOneElementInTab() {
-        int[] tab={1};
+    @Test public void presentInTabWithOneElement() {
+        int[] tab = {1};
         int key = 1;
-        SearchResult searchResult = BinarySearch.search(1,tab);
-        assertThat(searchResult.isFound(),is(true));
+        SearchResult searchResult = BinarySearch.search(key, tab);
+        assertThat(searchResult.isFound(), is(true));
+    }
+
+    @Test public void noPresentInTabWithOneElement() {
+        int[] tab = {2};
+        int key = 1;
+        SearchResult searchResult = BinarySearch.search(key, tab);
+        assertThat(searchResult.isFound(), is(false));
+    }
+
+    @Test public void presentInFirstPositionInTab() {
+        int[] tab = {1, 2, 3};
+        int key = 1;
+        SearchResult searchResult = BinarySearch.search(key, tab);
+        assertThat(searchResult.getPosition(), is(1));
+    }
+
+    @Test public void presentInLastPositionInTab() {
+        int[] tab = {1, 2, 4};
+        int key = 4;
+        SearchResult searchResult = BinarySearch.search(key, tab);
+        assertThat(searchResult.getPosition(), is(3));
+    }
+
+    @Test public void presentInCentrePositionInTab() {
+        int[] tab = {1, 2, 4};
+        int key = 2;
+        SearchResult searchResult = BinarySearch.search(key, tab);
+        assertThat(searchResult.getPosition(), is(2));
+    }
+
+    @Test public void noPresentInCentrePositionInTab() {
+        int[] tab = {1, 3, 4};
+        int key = 2;
+        SearchResult searchResult = BinarySearch.search(key, tab);
+        assertThat(searchResult.getPosition(), is(-1));
     }
 }
