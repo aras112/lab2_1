d68787db469d3efac0f0c6a2f2b8962a023e7f50
Caroolinnee <karolinab341@gmail.com>
2019-03-25 07:28:17 +0100

    Testy_v2_Added Big And Small Integers And Null

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -6,69 +6,117 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
-public class BinarySearchTest {
+public class BinarySearchTest
+    {
 
-    @Before public void setUp() throws Exception {
-    }
+    @Before
+    public void setUp() throws Exception
+        {
+        }
 
-    @Test public void presentInTabWithOneElement() {
+    @Test
+    public void presentInTabWithOneElement()
+        {
         int[] tab = {1};
         int key = 1;
         SearchResult searchResult = BinarySearch.search(key, tab);
         assertThat(searchResult.isFound(), is(true));
-    }
+        }
 
-    @Test public void noPresentInTabWithOneElement() {
+    @Test
+    public void noPresentInTabWithOneElement()
+        {
         int[] tab = {2};
         int key = 1;
         SearchResult searchResult = BinarySearch.search(key, tab);
         assertThat(searchResult.isFound(), is(false));
-    }
+        }
 
-    @Test public void presentInFirstPositionInTab() {
+    @Test
+    public void presentInFirstPositionInTab()
+        {
         int[] tab = {1, 2, 3};
         int key = 1;
         SearchResult searchResult = BinarySearch.search(key, tab);
         assertThat(searchResult.getPosition(), is(1));
-    }
+        }
 
-    @Test public void presentInLastPositionInTab() {
+    @Test
+    public void presentInLastPositionInTab()
+        {
         int[] tab = {1, 2, 4};
         int key = 4;
         SearchResult searchResult = BinarySearch.search(key, tab);
         assertThat(searchResult.getPosition(), is(3));
-    }
+        }
 
-    @Test public void presentInCentrePositionInTab() {
+    @Test
+    public void presentInCentrePositionInTab()
+        {
         int[] tab = {1, 2, 4};
         int key = 2;
         SearchResult searchResult = BinarySearch.search(key, tab);
         assertThat(searchResult.getPosition(), is(2));
-    }
+        }
 
-    @Test public void noPresentInCentrePositionInTab() {
+    @Test
+    public void noPresentInCentrePositionInTab()
+        {
         int[] tab = {1, 3, 4};
         int key = 2;
         SearchResult searchResult = BinarySearch.search(key, tab);
         assertThat(searchResult.getPosition(), is(-1));
-    }
+        }
 
-    @Test public void bigIntegersInTab() {
-        int[] tab = {Integer.MAX_VALUE-3, Integer.MAX_VALUE-2, Integer.MAX_VALUE};
+    @Test
+    public void bigIntegersInTab()
+        {
+        int[] tab = {Integer.MAX_VALUE - 3, Integer.MAX_VALUE - 2, Integer.MAX_VALUE};
         int key = Integer.MAX_VALUE;
         SearchResult searchResult = BinarySearch.search(key, tab);
         assertThat(searchResult.getPosition(), is(3));
-    }
+        }
 
-    @Test public void emptyList() {
+    @Test
+    public void minIntegersInTab()
+        {
+        int[] tab = {Integer.MIN_VALUE, Integer.MIN_VALUE + 2, Integer.MIN_VALUE+3};
+        int key = Integer.MIN_VALUE;
+        SearchResult searchResult = BinarySearch.search(key, tab);
+        assertThat(searchResult.getPosition(), is(1));
+        }
+
+    @Test
+    public void emptyList()
+        {
         int[] tab = {};
         int key = Integer.MAX_VALUE;
         SearchResult searchResult = BinarySearch.search(key, tab);
-        assertTrue(searchResult.getPosition()==-1&&searchResult.isFound()==false);
-    }
-    @Test(expected = NullPointerException.class) public void nullTab() {
+        assertTrue(searchResult.getPosition() == -1 && searchResult.isFound() == false);
+        }
+
+    @Test(expected = NullPointerException.class)
+    public void nullKeyInTab()
+        {
+        int[] tab = {};
+        Integer key = null;
+        SearchResult searchResult = BinarySearch.search(key, tab);
+        }
+
+    @Test(expected = NullPointerException.class)
+    public void nullTabInTab()
+        {
         int[] tab = null;
         int key = Integer.MAX_VALUE;
         SearchResult searchResult = BinarySearch.search(key, tab);
+        }
+
+    @Test(expected = NullPointerException.class)
+    public void nullKeyAndTabInTab()
+        {
+        int[] tab = null;
+        Integer key = null;
+        SearchResult searchResult = BinarySearch.search(key, tab);
+        }
+
     }
-}
