f50c6e1038008bb608002812b1048c6e7f7d108d
Caroolinnee <karolinab341@gmail.com>
2019-04-08 19:59:46 +0200

    Testy_v3_Added add BinarySearch class now it works -_-

--- src/main/java/edu/iis/mto/bsearch/BinarySearch.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearch.java
@@ -3,13 +3,25 @@
  */
 package edu.iis.mto.bsearch;
 
+import com.google.common.collect.Ordering;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Utility Class dla wyszukiwania binarnego
  *
  */
-public class BinarySearch {
+public class BinarySearch
+    {
 
-    private BinarySearch() {}
+    private BinarySearch()
+        {
+        }
 
     /**
      * Metoda realizujaca wyszukiwanie binarne
@@ -21,27 +33,51 @@ public class BinarySearch {
      * @return obiekt rezultatu o polach: - found (true jezeli znaleziony) - position (jezeli znaleziony - pozycja w
      *         sekwencji, jezeli nie znaleziony -1)
      */
-    public static SearchResult search(int key, int[] seq) {
+    public static SearchResult search(int key, int[] seq)
+        {
+
+        /*Guava needed*/
+        checkIsOrdered(seq);
+
         int start = 0;
         int end = seq.length - 1;
         int center;
         SearchResult result = new SearchResult();
 
-        while (start <= end) {
+        while (start <= end)
+            {
             center = (start + end) / 2;
-            if (seq[center] == key) {
+            if (seq[center] == key)
+                {
                 result.setPosition(center + 1);
                 break;
-            } else {
-                if (seq[center] < key) {
-                    start = center + 1;
-                } else {
-                    end = center - 1;
                 }
+            else
+                {
+                if (seq[center] < key)
+                    {
+                    start = center + 1;
+                    }
+                else
+                    {
+                    end = center - 1;
+                    }
+                }
+
             }
-
-        }
         return result;
-    }
+        }
 
-}
+    private static void checkIsOrdered(int[] seq)
+        {
+
+        List<Integer> list = Arrays.stream(seq).boxed().collect(Collectors.toList());
+
+
+        if (!Ordering.natural().isOrdered(list))
+            {
+            throw new IllegalArgumentException("List must be sorted");
+            }
+        }
+
+    }
